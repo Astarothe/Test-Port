059820c649c752f350f6e868b9da3059aab866fe
Astaroth <minatosavich@gmail.com>
2021-10-09 19:13:34 +0300

    fix: button link hash

--- src/common/Button/Button.tsx
+++ src/common/Button/Button.tsx
@@ -2,6 +2,7 @@ import s from "./Button.module.scss"
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import React, {FC} from "react";
 import {IconProp} from "@fortawesome/fontawesome-svg-core";
+import { NavLink } from "react-router-dom";
 
 type ButtonType = {
     icon: IconProp
@@ -20,12 +21,12 @@ export const Button: FC<ButtonType> = ({icon, link, type, children}) => {
                             <FontAwesomeIcon icon={icon} color={"white"} size={"1x"} className={s.icon}/>
                     </span>
                 </button>
-                : <a className={s.button} href={link || "#"}>
+                : <NavLink className={s.button} to={link || "#"}>
                     <span>{children}</span>
                     <span className={s.buttonIcon}>
                             <FontAwesomeIcon icon={icon} color={"white"} size={"1x"} className={s.icon}/>
                     </span>
-                </a>}
+                </NavLink>}
 
         </>
     )
